e7d2b041d13ac4d22e89fa04c773ab8f7c1305d8
Vinay Srinivasan <vsrin008@ucr.edu>
2023-12-11 21:49:11 -0800

    Login system working
    Login system working and will alert after a successful login

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,36 @@
-from flask import Flask, request, redirect
+import os
+from flask import Flask, request, redirect, send_from_directory
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('html-boilerplate', 'index.html')
+    html_boilerplate_dir = os.path.abspath('../html-boilerplate/pages')
+    return send_from_directory(html_boilerplate_dir, 'index.html')
 
 @app.route('/login')
 def login():
-    # This is where you'd serve your login.html
-    return send_from_directory('html-boilerplate', 'login.html')
+    html_boilerplate_pages_dir = os.path.abspath('../html-boilerplate/pages')
+    return send_from_directory(html_boilerplate_pages_dir, 'login.html')
+
+@app.route('/about')
+def about():
+    html_boilerplate_pages_dir = os.path.abspath('../html-boilerplate/pages')
+    return send_from_directory(html_boilerplate_pages_dir, 'about.html')
+
+
+@app.route('/images/<filename>')
+def images(filename):
+    images_dir = os.path.abspath('../html-boilerplate/images')
+    return send_from_directory(images_dir, filename)
+
+@app.route('/stylesheets/<filename>')
+def stylesheet(filename):
+    stylesheets_dir = os.path.abspath('../html-boilerplate/stylesheets')
+    return send_from_directory(stylesheets_dir, filename)
+
+
 
 @app.route('/callback')
 def callback():
@@ -18,13 +38,12 @@ def callback():
     response = requests.post('https://accounts.spotify.com/api/token', {
         'grant_type': 'authorization_code',
         'code': code,
-        'redirect_uri': 'http://localhost:8080/callback',
+        'redirect_uri': 'http://127.0.0.1:8080/callback',
         'client_id': '4f8a0448747a497e99591f5c8983f2d7',
         'client_secret': 'b7ba599280d64d2ab32cf9cc9cbec47a'
     })
     access_token = response.json().get('access_token')
-    # Handle the access token (store it, create session, etc.)
-    return redirect('/some_page_after_login')  # Redirect to a different page after login
+    return redirect('/?login=success')  # Redirect to a different page after login
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
