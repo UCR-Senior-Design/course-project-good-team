7d8619a2fa87f6be0edac150d77a950878986093
Vincent Martinez <vmart044@ucr.edu>
2023-12-11 21:54:36 -0800

    Updating files

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, redirect
+import os
+from flask import Flask, request, redirect, send_from_directory
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('html-boilerplate', 'index.html')
+    html_boilerplate_dir = os.path.abspath('../html-boilerplate')
+    return send_from_directory(html_boilerplate_dir, 'index.html')
 
 @app.route('/login')
 def login():
-    # This is where you'd serve your login.html
-    return send_from_directory('html-boilerplate', 'login.html')
+    html_boilerplate_pages_dir = os.path.abspath('../html-boilerplate/pages')
+    return send_from_directory(html_boilerplate_pages_dir, 'login.html')
 
 @app.route('/callback')
 def callback():
@@ -18,13 +20,12 @@ def callback():
     response = requests.post('https://accounts.spotify.com/api/token', {
         'grant_type': 'authorization_code',
         'code': code,
-        'redirect_uri': 'http://localhost:8080/callback',
+        'redirect_uri': 'http://127.0.0.1:5000/callback',
         'client_id': '4f8a0448747a497e99591f5c8983f2d7',
         'client_secret': 'b7ba599280d64d2ab32cf9cc9cbec47a'
     })
     access_token = response.json().get('access_token')
-    # Handle the access token (store it, create session, etc.)
-    return redirect('/some_page_after_login')  # Redirect to a different page after login
+    return redirect('/login')  # Redirect to a different page after login
 
 if __name__ == '__main__':
     app.run()
