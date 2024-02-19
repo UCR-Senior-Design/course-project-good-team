af70b361ff911ad5cfe30c983d771fa61bb71c46
Vinay Srinivasan <vsrin008@ucr.edu>
2024-02-19 14:52:09 -0800

    Created profiles for users
    Can now see profiles for your friends, accessed from the friends page

--- db_utils.py
+++ db_utils.py
@@ -1,22 +1,24 @@
-def update_user_document(users, userid, username, profile_pic_url, top_data, playlistsnameid):
-    # Check if the user exists; if not, insert new, otherwise update
+from datetime import datetime
+
+def update_user_document(users, userid, username, profile_pic_url, top_data, playlistsnameid, date_joined=None):
     existing_user = users.find_one({'id': userid})
 
     if existing_user:
-        # User exists, update their profile including top tracks and artists
-        users.update_one(
-            {'id': userid},
-            {'$set': {
-                'profile_pic_url': profile_pic_url,
-                **top_data  # Unpack top data dictionary to set additional fields
-            }}
-        )
+        # For existing users, only update if there's new data; don't overwrite date_joined
+        update_data = {
+            'profile_pic_url': profile_pic_url,
+            **top_data  # Unpack top data dictionary
+        }
+        users.update_one({'id': userid}, {'$set': update_data})
     else:
-        # User doesn't exist, insert as new
+        # For new users, add date_joined
+        if date_joined is None:
+            date_joined = datetime.now().strftime('%Y-%m-%d')  # Format: YYYY-MM-DD
         new_user = {
             'id': userid,
             'username': username,
             'profile_pic_url': profile_pic_url,
+            'date_joined': date_joined,
             'friends': [],
             'friendRequests': [],
             'playlists': playlistsnameid,
